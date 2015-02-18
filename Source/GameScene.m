86861f5e75a05b39fa479d341502777137a0b796
thefreshduke <scottyshaw1@gmail.com>
2015-02-18 17:41:31 +0000

    Simplified color UX; commented out previous color UX code for future use

--- Source/GameScene.m
+++ Source/GameScene.m
@@ -26,19 +26,20 @@ static double MAX_HOLD_TIME = 0.2;
 - (void)didLoadFromCCB {
     self.userInteractionEnabled = true;
     [self setDashOrDot];
+    _background.color = [CCColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0f];
 }
 
 - (void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    _background.color = [CCColor colorWithRed:255.0f/255.0f green:0.0/255.0f blue:255.0/255.0f alpha:1.0f];
     [self initHoldTimer];
+    _background.color = [CCColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0f];
 }
 
 - (void) touchEnded: (UITouch *) touch withEvent: (UIEvent *) event {
-    _background.color = [CCColor colorWithRed:255.0f/255.0f green:0.0/255.0f blue:0.0/255.0f alpha:1.0f];
     if (_timer.isValid) {
         if ((_seconds >= MAX_HOLD_TIME && [_dashLabel.string isEqualToString:@"_"]) ||
             (_seconds < MAX_HOLD_TIME && [_dotLabel.string isEqualToString:@"."])) {
             score++;
+            _background.color = [CCColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0f];
         }
         else {
             NSLog(@"Your score is %d", score);
@@ -71,16 +72,18 @@ static double MAX_HOLD_TIME = 0.2;
     }
 }
 
+-(void) update: (CCTime) delta {
+    _seconds += delta;
+//    [self updateBackgroundColor];
+}
+
 - (void) updateBackgroundColor {
     CGFloat redness = (4.0 * pow(_seconds, 2.0)) - (4.0 * _seconds) + 1.0;
     CGFloat greenness = (-4.0 * pow(_seconds, 2.0)) + (4.0 * _seconds);
+//    CGFloat redness = abs((2.0 * _seconds) - 1.0);
+//    CGFloat greenness = -abs(2.0 * _seconds - 1.0) + 1.0;
     CGFloat blueness = (-2.0 * _seconds) + 1.0;
     _background.color = [CCColor colorWithRed:redness green:greenness blue:blueness alpha:1.0f];
-}
-
--(void) update: (CCTime) delta {
-    _seconds += delta;
-    [self updateBackgroundColor];
 }
 
 - (void) loadMainScene {
