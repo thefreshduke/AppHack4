dd2f90b2d50c208ddd5286521ff7d2297fdf3335
thefreshduke <scottyshaw1@gmail.com>
2015-02-22 20:17:34 +0000

    Score labels in recapScene display properly; code in GameScene.m needs to be cleaned up; high scores not displaying correct numbers yet

--- Source/GameScene.m
+++ Source/GameScene.m
@@ -43,24 +43,24 @@ static NSString *blank = @" ";
     _score = 0;
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change
-                       context:(void *)context {
+- (void) observeValueForKeyPath: (NSString *) keyPath ofObject: (id) object change: (NSDictionary *) change
+                       context: (void *) context {
     if ([keyPath isEqualToString:@"score"]) {
         _scoreLabel.string = [NSString stringWithFormat:@"%ld", (long)_score];
     }
-    else if ([keyPath isEqualToString:@"highScore"]) {
+    else if ([keyPath isEqualToString: @"highScore"]) {
         [_recapScene updateHighScore];
     }
 }
 
-- (void)dealloc {
-    [self removeObserver:self forKeyPath:@"score"];
+- (void) dealloc {
+    [self removeObserver:self forKeyPath: @"score"];
 }
 
-- (void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+- (void) touchBegan:(UITouch *) touch withEvent: (UIEvent *) event {
     [self initHoldTimer];
     _seconds = 0.0;
-    _background.color = [CCColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0f];
+    _background.color = [CCColor colorWithRed: 1.0 green: 1.0 blue: 1.0 alpha: 1.0f];
 }
 
 - (void) touchEnded: (UITouch *) touch withEvent: (UIEvent *) event {
@@ -68,7 +68,7 @@ static NSString *blank = @" ";
         if ((_seconds >= DOT_DASH_DELIMITER && [_dashLabel.string isEqualToString:dash]) ||
             (_seconds < DOT_DASH_DELIMITER && [_dotLabel.string isEqualToString:dot])) {
             _score++;
-            _background.color = [CCColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0f];
+            _background.color = [CCColor colorWithRed: 0.0 green: 1.0 blue: 0.0 alpha: 1.0f];
             _background.endOpacity -= 0.01;
             [self setDashOrDot];
         }
@@ -84,7 +84,7 @@ static NSString *blank = @" ";
 
 - (void) initHoldTimer {
     _seconds = 0.0;
-    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(hold:) userInfo:nil repeats:YES];
+    _timer = [NSTimer scheduledTimerWithTimeInterval: 1.0 target: self selector: @selector (hold:) userInfo: nil repeats: YES];
 }
 
 -(void) hold: (NSTimer *) sendingTimer {
@@ -109,7 +109,7 @@ static NSString *blank = @" ";
     if (_timer.isValid && _seconds >= MAX_DELAY) {
         [self endGame];
     }
-    _scoreLabel.string = [NSString stringWithFormat:@"%ld", (long)_score];
+    _scoreLabel.string = [NSString stringWithFormat: @"%ld", (long) _score];
 }
 
 - (void) updateBackgroundColor {
@@ -126,9 +126,17 @@ static NSString *blank = @" ";
     _background.color = [CCColor colorWithRed: 1.0 green: 0.0 blue: 0.0 alpha: 1.0f];
     NSLog(@"Your score is %d", _score);
 //    [self setHighScore];
-    [_recapScene setScore: _score];
-    [self scheduleOnce: @selector(loadRecapScene) delay: 1.0];
+//    [_recapScene setScore: _score];
+//    [self scheduleOnce: @selector(loadRecapScene) delay: 1.0];
     [_timer invalidate];
+    
+//    [self setHighScore];
+    RecapScene *recapScene = (RecapScene*) [CCBReader load:@"RecapScene"];
+    [recapScene setScore: self->_score];
+    CCScene *newScene = [CCScene node];
+    [newScene addChild:recapScene];
+    CCTransition *transition = [CCTransition transitionFadeWithDuration:1.0f];
+    [[CCDirector sharedDirector] presentScene:newScene withTransition:transition];
 }
 
 //- (void) setHighScore {
@@ -143,12 +151,12 @@ static NSString *blank = @" ";
 
 - (void) loadMainScene {
     CCScene *mainScene = [CCBReader loadAsScene: @"MainScene"];
-    [[CCDirector sharedDirector] replaceScene:mainScene];
+    [[CCDirector sharedDirector] replaceScene: mainScene];
 }
 
 - (void) loadRecapScene {
     CCScene *recapScene = [CCBReader loadAsScene: @"RecapScene"];
-    [[CCDirector sharedDirector] replaceScene:recapScene];
+    [[CCDirector sharedDirector] replaceScene: recapScene];
 }
 
 @end
--- Source/RecapScene.m
+++ Source/RecapScene.m
@@ -14,27 +14,34 @@
     CCNodeGradient *_background;
     CCLabelTTF *_highScoreLabel;
     CCLabelTTF *_scoreLabel;
+    BOOL highScore;
 }
 
-- (void)setScore:(NSInteger)score {
+- (void) didLoadFromCCB {
+    highScore = false;
+    [self updateHighScore];
+}
+
+
+- (void) setScore: (NSInteger) score {
     //    if (!highScore) {
-    _scoreLabel.string = [NSString stringWithFormat:@"%ld", (long)score];
+    _scoreLabel.string = [NSString stringWithFormat:@"%ld", (long) score];
     //    } //how to shift between standard "your score/best score" and "YOU GOT A HIGH SCORE"?
 }
 
-- (void)updateHighScore {
-    NSInteger newHighScore = [[NSUserDefaults standardUserDefaults] integerForKey:@"HighScore"];
-    _highScoreLabel.string = [NSString stringWithFormat:@"%d", (int) newHighScore];
+- (void) updateHighScore {
+    NSInteger newHighScore = [[NSUserDefaults standardUserDefaults] integerForKey: @"HighScore"];
+    _highScoreLabel.string = [NSString stringWithFormat: @"%d", (int) newHighScore];
 }
 
-- (void)loadMainScene {
-    CCScene *mainScene = [CCBReader loadAsScene:@"MainScene"];
+- (void) loadMainScene {
+    CCScene *mainScene = [CCBReader loadAsScene: @"MainScene"];
     [[CCDirector sharedDirector] replaceScene:mainScene];
 }
 
-- (void)loadGameScene {
-    CCScene *gameScene = [CCBReader loadAsScene:@"GameScene"];
-    [[CCDirector sharedDirector] replaceScene:gameScene];
+- (void) loadGameScene {
+    CCScene *gameScene = [CCBReader loadAsScene: @"GameScene"];
+    [[CCDirector sharedDirector] replaceScene: gameScene];
 }
 
 @end
